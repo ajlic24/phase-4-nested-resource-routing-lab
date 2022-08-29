0250a9626a1d73d2dda76ae0aaf829b8ef438ebc
ajlic24@gmail.com <ajlic24@gmail.com>
2022-08-29 05:48:49 -0400

    commit tree c4ac401beebe58c053a48f87651503275d12d3cc

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      items = find_user
+      render json: items.items, status: :ok
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item, status: :ok
+  end
+
+  def create
+    user = find_user
+    created = user.items.create(items_params)
+    render json: created, status: :created
+  end
+
+  private 
+
+  def items_params
+    params.permit(:name, :description, :price)
+  end
+
+  def find_user
+    User.find(params[:user_id])
+  end
+
+  def render_not_found_response
+    render json: {error: 'user not found'}, status: :not_found
+  end
 end
